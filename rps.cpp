50a30c57ec25f9781c598916808abc7486e83ad6
manitofighh <manitofigh@protonmail.com>
2023-03-26 13:15:23 -0400

    added final functionality

--- rps.cpp
+++ rps.cpp
@@ -12,11 +12,79 @@ string computerChoice(){
     int computerChoice = rand() % 3 + 1;
     if (computerChoice == 1) return "Rock";
     else if (computerChoice == 2) return "Paper";
-    else return "Scissor";
+    else return "Scissors";
 }
 
 void checkResult(){
-    if (choice == "rock" && computerChoice() == "")
+    if (choice == "rock" && computerChoice() == "Rock"){
+        cout << "\nComputer's choice: Rock" << endl;
+        cout << "Your choice: Rock" << endl;
+        cout << "No winner for this round!";
+        roundNumber ++;
+    }
+    else if (choice == "rock" && computerChoice() == "Paper"){
+        cout << "\nComputer's choice: Paper" << endl;
+        cout << "Your choice: Rock" << endl;
+        cout << "Computer won this round!" << endl;
+        roundNumber ++;
+        botScore ++;
+    }
+    else if (choice == "rock" && computerChoice() == "Scissors"){
+        cout << "\nComputer's choice: Scissors" << endl;
+        cout << "Your choice: Rock" << endl;
+        cout << "You won this round!" << endl;
+        roundNumber ++;
+        userScore ++;
+    }
+
+    else if (choice == "paper" && computerChoice() == "Rock") {
+        cout << "\nComputer's choice: Rock" << endl;
+        cout << "Your choice: Paper" << endl;
+        cout << "You won this round!" << endl;
+        roundNumber ++;
+        userScore ++;
+    }
+
+    else if (choice == "paper" && computerChoice() == "Paper") {
+        cout << "\nComputer's choice: Paper" << endl;
+        cout << "Your choice: Paper" << endl;
+        cout << "No winner for this round!";
+        roundNumber ++;
+    }
+
+    else if (choice == "paper" && computerChoice() == "Scissors"){
+        cout << "\nComputer's choice: Paper" << endl;
+        cout << "Your choice: Scissors" << endl;
+        cout << "Computer won this round!" << endl;
+        roundNumber ++;
+        botScore ++;
+    }
+
+    else if (choice == "scissors" && computerChoice() == "Rock"){
+        cout << "\nComputer's choice: Rock" << endl;
+        cout << "Your choice: Scissors" << endl;
+        cout << "Computer won this round!" << endl;
+        roundNumber ++;
+        botScore ++;
+    }
+
+    else if (choice == "scissors" && computerChoice() == "Paper"){
+        cout << "\nComputer's choice: Paper" << endl;
+        cout << "Your choice: Scissors" << endl;
+        cout << "You won this round!" << endl;
+        roundNumber ++;
+        userScore ++;
+    }
+
+    else if (choice == "scissors" && computerChoice() == "Scissors"){
+        cout << "\nComputer's choice: Scissors" << endl;
+        cout << "Your choice: Scissors" << endl;
+        cout << "No winner for this round!";
+        roundNumber ++;
+    }
+
+    else cout << "Your input could not be validated" << endl;
+
 }
 
 int main(){
@@ -42,10 +110,19 @@ int main(){
     cin >> choice;
     transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
     checkResult();
-
     }
     while (userScore != 3 || botScore != 3);
 
-    
+    if (userScore == 3){
+        cout << "**************" << endl;
+        cout << "** You Won! **" << endl;
+        cout << "**************" << endl;
+    }
+
+    else if (botScore == 3) {
+        cout << "***************" << endl;
+        cout << "** You Lost! **" << endl;
+        cout << "***************" << endl;
+    }
 
 }
